c0756e473a95c07081e5bc337b2948718fe63e7c
Alexs06 <49921375+Alexs06@users.noreply.github.com>
2019-06-26 15:00:49 -0700

    Add files via upload

---
+++ java/EmailLoader.java
@@ -0,0 +1,11 @@
+import com.google.api.services.gmail.Gmail;
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+public class EmailLoader {
+
+    public ArrayList<Email> getNewMail(Gmail service, String label) throws IOException {
+        Gmail.Users.Messages.List request 
+    }
+}
